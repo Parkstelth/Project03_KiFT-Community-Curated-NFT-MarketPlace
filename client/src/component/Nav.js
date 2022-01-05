34d8eb5dc4e1d5114464b4c05342936f991873a1
sonny6786 <3076270s@gmail.com>
2022-01-05 10:41:49 +0900

    working nav bar

--- client/src/component/Nav.js
+++ client/src/component/Nav.js
@@ -1,24 +1,33 @@
 import styled from "styled-components";
 const TopHeader = styled.header`
-    position: fixed;
-    top: 0;
+    position: relative;
+    left: 0px;
     width: 100%;
     height: 80px;
 `;
 const TopHeaderWrap = styled.div`
     display: flex;
+    flex-direction: row;
     align-items: center;
-    justify-content: center;
+    justify-content: space-between;
+
     height: 100%;
-    max-width: 1400px;
+    max-width: 1000px;
     margin: 0 auto;
 `;
-const TopHeaderLogo = styled.h1``;
-const Input = styled.input``;
-const TopHeaderNav = styled.ul`
-    display: flex;
+const TopHeaderLogo = styled.h1`
+    flex: 1 1 auto;
+    border: 1px solid black;
 `;
-const TopHeaderNavList = styled.li``;
+const Input = styled.input`
+    flex: 1 1 auto;
+`;
+const TopHeaderNav = styled.div`
+    display: flex;
+    border: 1px solid black;
+    flex: 3 1 auto;
+`;
+const TopHeaderNavList = styled.div``;
 function Nav() {
     return (
         <TopHeader>
