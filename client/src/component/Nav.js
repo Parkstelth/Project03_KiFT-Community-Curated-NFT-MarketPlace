fdacb63fe71ea08883aaf3a57fe0ad0dc8713aee
sonny6786 <3076270s@gmail.com>
2022-01-05 15:59:53 +0900

    modified front web

--- client/src/component/Nav.js
+++ client/src/component/Nav.js
@@ -6,8 +6,8 @@ function Nav() {
             <div className="NavBarInisde">
                 <div className="NavBarInisdeContainer">
                     <div className="homeNav">KiFT</div>
-                    <div className="nav">market</div>
-                    <div className="nav">Features</div>
+                    <div className="nav">Market</div>
+                    <div className="nav">Curated</div>
                     <div className="nav">Community</div>
                     <div className="nav">Search</div>
                     <div className="nav">Sign in</div>
--- client/src/component/frontpage.js
+++ client/src/component/frontpage.js
@@ -1,50 +1,44 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import "./frontpage.css";
 
 const FrontStart = styled.div`
-    margin-top:80px;
+    margin-top: 80px;
 `;
 
 const FrontStartWrap = styled.div`
-    display:flex;
+    display: flex;
     justify-content: center;
 `;
 
 const LeftSource = styled.div`
-    display:flex;
+    display: flex;
     flex-direction: column;
-    padding-left:11vw;
-
+    padding-left: 11vw;
 `;
 
 const RightSource = styled.div`
-    display:flex;
+    display: flex;
     flex-direction: column;
-    max-width:412px;
-    max-height:517px;
-    padding:0px 11em 0px 4%;
-    
+    max-width: 412px;
+    max-height: 517px;
+    padding: 0px 11em 0px 4%;
 `;
-
-
 
 function FrontPage() {
     return (
         <FrontStart>
             <FrontStartWrap>
-            <LeftSource>
-            <div>CURATOR ART</div>
-            <img className="mainart" src="https://ipfs.pixura.io/ipfs/QmPVXTbzi83fDYHU6MNcHTJC3aYnkME5AZBvBDjeCUNnEg/Somber.jpg"></img>
-            </LeftSource>
-            <RightSource>
-                <div className="mainstart">
-            <div className="h1">collet</div>
-            <div className="h1">digital art</div>
-                </div>
-
-            </RightSource>
-
+                <LeftSource>
+                    <div>CURATOR ART</div>
+                    <img className="mainart" src="https://ipfs.pixura.io/ipfs/QmPVXTbzi83fDYHU6MNcHTJC3aYnkME5AZBvBDjeCUNnEg/Somber.jpg"></img>
+                </LeftSource>
+                <RightSource>
+                    <div className="mainstart">
+                        <div className="h1">collect</div>
+                        <div className="h1">digital art</div>
+                    </div>
+                </RightSource>
             </FrontStartWrap>
         </FrontStart>
     );
