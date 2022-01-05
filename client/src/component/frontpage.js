e56542b39761abb4322b3bfac06f179702eae917
Parkstelth <stelthicmaster@gmail.com>
2022-01-05 16:03:10 +0900

    merge 03

--- client/src/component/frontpage.js
+++ client/src/component/frontpage.js
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import styled from "styled-components";
 import "./frontpage.css";
+import { useEffect } from 'react';
 
 const FrontStart = styled.div`
     margin-top:80px;
@@ -23,26 +24,42 @@ const RightSource = styled.div`
     flex-direction: column;
     max-width:412px;
     max-height:517px;
-    padding:0px 11em 0px 4%;
+    padding:35px 1em 0px 4%;
     
 `;
 
 
 
+
+
 function FrontPage() {
+    
     return (
-        <FrontStart>
+        <FrontStart >
             <FrontStartWrap>
-            <LeftSource>
-            <div>CURATOR ART</div>
-            <img className="mainart" src="https://ipfs.pixura.io/ipfs/QmPVXTbzi83fDYHU6MNcHTJC3aYnkME5AZBvBDjeCUNnEg/Somber.jpg"></img>
+            <LeftSource >
+            <div className="title">CURATOR ART</div>
+            <img className="introImg" src="https://ipfs.pixura.io/ipfs/QmPVXTbzi83fDYHU6MNcHTJC3aYnkME5AZBvBDjeCUNnEg/Somber.jpg"></img>
+            <div className="description">
+             
+                    <div className="artist">
+                        <div>ARTIST</div>
+                        <div className="artist_data">{'@park-son'}</div>
+                    </div>
+                    <div className="release_date">
+                        <div className="date_title">RELEASE DATE</div>
+                        <div className="date"> {'Dec22,2021,12pm'}</div>
+                    </div>
+            </div>
             </LeftSource>
             <RightSource>
                 <div className="mainstart">
             <div className="h1">collet</div>
             <div className="h1">digital art</div>
+            <p className="h2">Buy and sell NFTs from the</p>
+            <p className="h2">world's top artists</p>
+            <button className="startbutton">START COLLECTING</button>
                 </div>
-
             </RightSource>
 
             </FrontStartWrap>
