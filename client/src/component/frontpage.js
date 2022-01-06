1c8e4ad3b7fc2a88afd174a08d54836b8efaa1d7
Parkstelth <stelthicmaster@gmail.com>
2022-01-06 09:40:57 +0900

    merge 06

--- client/src/component/frontpage.js
+++ client/src/component/frontpage.js
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import "./frontpage.css";
-import { useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.css'
+import { Carousel } from "react-bootstrap";
+import {useState } from "react";
+import main_image_01 from "../image/main.jpg"
 
 const FrontStart = styled.div`
     margin-top: 80px;
@@ -32,38 +35,80 @@ const RightSource = styled.div`
 
 
 function FrontPage() {
-    
-    return (
-        <FrontStart >
-            <FrontStartWrap>
-            <LeftSource >
-            <div className="title">CURATOR ART</div>
-            <img className="introImg" src="https://ipfs.pixura.io/ipfs/QmPVXTbzi83fDYHU6MNcHTJC3aYnkME5AZBvBDjeCUNnEg/Somber.jpg"></img>
-            <div className="description">
-             
-                    <div className="artist">
-                        <div>ARTIST</div>
-                        <div className="artist_data">{'@park-son'}</div>
-                    </div>
-                    <div className="release_date">
-                        <div className="date_title">RELEASE DATE</div>
-                        <div className="date"> {'Dec22,2021,12pm'}</div>
-                    </div>
-            </div>
-            </LeftSource>
-            <RightSource>
-                <div className="mainstart">
-            <div className="h1">collet</div>
-            <div className="h1">digital art</div>
-            <p className="h2">Buy and sell NFTs from the</p>
-            <p className="h2">world's top artists</p>
-            <button className="startbutton">START COLLECTING</button>
-                </div>
-            </RightSource>
+const [index, setIndex] = useState(0);
 
-            </FrontStartWrap>
-        </FrontStart>
-    );
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <div>
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item >
+        <img
+          className="d-block w-100"
+          src={main_image_01}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item >
+        <img
+          className="d-block w-100"
+          src={main_image_01}
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item >
+        <img
+          className="d-block w-100"
+          src={main_image_01}
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+     <FrontStart >
+            
+     <FrontStartWrap>
+     <LeftSource >
+     <div className="title">CURATOR ART</div>
+     <img className="introImg" src="https://ipfs.pixura.io/ipfs/QmPVXTbzi83fDYHU6MNcHTJC3aYnkME5AZBvBDjeCUNnEg/Somber.jpg"></img>
+     <div className="description">
+      
+             <div className="artist">
+                 <div>ARTIST</div>
+                 <div className="artist_data">{'@park-son'}</div>
+             </div>
+             <div className="release_date">
+                 <div className="date_title">RELEASE DATE</div>
+                 <div className="date"> {'Dec22,2021,12pm'}</div>
+             </div>
+     </div>
+     </LeftSource>
+     <RightSource>
+         <div className="mainstart">
+     <div className="h1">collet</div>
+     <div className="h1">digital art</div>
+     <p className="h2">Buy and sell NFTs from the</p>
+     <p className="h2">world's top artists</p>
+     <button className="startbutton">START COLLECTING</button>
+         </div>
+     </RightSource>
+
+     </FrontStartWrap>
+ </FrontStart>
+ </div>
+  );
 }
+
+         
+        
+   
+
 
 export default FrontPage;
