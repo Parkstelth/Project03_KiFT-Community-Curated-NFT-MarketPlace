be84086874bd24b8334802d93937d2d60d8ae4c3
sonny6786 <3076270s@gmail.com>
2022-01-10 17:59:48 +0900

    working signin page and app.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,60 +7,56 @@ import SignIn from "./component/SignIn";
 import NotFound from "./component/notfound";
 import Nav from "./component/Nav";
 import Footer from "./component/Footer";
-import { useState,useEffect } from 'react'
-import Web3 from 'web3'
+import { useState, useEffect } from "react";
+import Web3 from "web3";
 
 function App() {
+    const [footer, setFooter] = useState(true);
+    const [loginAccount, setLoginAccount] = useState("");
+    const [web3, setWeb3] = useState([]);
+    const [isLogin, setIsLogin] = useState(false);
 
-    const [footer,setFooter] = useState(true)
-    const [loginAccount,setLoginAccount] = useState('')
-    const [web3,setWeb3] = useState([])
-
-   
-    function setfooter(e){
-        setFooter(e)
+    function setfooter(e) {
+        setFooter(e);
     }
-   
-    useEffect(async() => {
-        if (typeof window.ethereum !== 'undefined') {
-          // window.ethereum이 있다면
-          try {
-              const web = new Web3(window.ethereum);
-              await web.eth.getAccounts().then((account)=>{
-                  setLoginAccount(account)
-              })
-              setWeb3(web)
-          } catch (err) {
-            console.log(err);
-          }
-        }
-        else{
-            setLoginAccount('')
-            setWeb3([])
-        }
-      }, []);
 
+    useEffect(async () => {
+        if (typeof window.ethereum !== "undefined") {
+            // window.ethereum이 있다면
+            try {
+                const web = new Web3(window.ethereum);
+                await web.eth.getAccounts().then((account) => {
+                    setLoginAccount(account);
+                });
+                setWeb3(web);
+            } catch (err) {
+                console.log(err);
+            }
+        } else {
+            setLoginAccount("");
+            setWeb3([]);
+        }
+    }, []);
 
-      console.log('text',loginAccount)
-      console.log('text2',web3)
+    console.log("text", loginAccount);
+    console.log("text2", web3);
 
     return (
-        
         <BrowserRouter>
-     
-            <Nav />
+            <Nav isLogin={isLogin} />
             <Routes>
-                <Route exact path="/" element={<FrontPage setfooter={setfooter}/>} />
-                <Route path="/market" element={<Market setfooter={setfooter}/>} />
-                <Route path="/signin" element={<SignIn setfooter={setfooter} setLoginAccount={setLoginAccount} setWeb3={setWeb3}/>} />
-              
+                <Route exact path="/" element={<FrontPage setfooter={setfooter} />} />
+                <Route path="/market" element={<Market setfooter={setfooter} />} />
+                {/* 로그인 시 마켓으로 이동하게 해놨음! 다른 곳으로 원하면
+                바꿔도 됨*/}
+                {!isLogin ? (
+                    <Route path="/signin" element={<SignIn setfooter={setfooter} setIsLogin={setIsLogin} setLoginAccount={setLoginAccount} setWeb3={setWeb3} />} />
+                ) : (
+                    <Route path="/market" element={<Market setfooter={setfooter} />} />
+                )}
             </Routes>
-            {
-                footer ? <Footer /> : null
-            }
-         
+            {footer ? <Footer /> : null}
         </BrowserRouter>
-        
     );
 }
 
--- client/src/component/SignIn.js
+++ client/src/component/SignIn.js
@@ -1,42 +1,82 @@
 import "./SignIn.css";
-import { useState,useEffect } from 'react'
-import Web3 from 'web3'
+import { useState, useEffect } from "react";
+import Web3 from "web3";
+import axios from "axios";
 
+function SignIn({ setfooter, setLoginAccount, setWeb3, setIsLogin }) {
+    useEffect(() => {
+        setfooter(false);
+    }, []);
 
-function SignIn({setfooter,setLoginAccount,setWeb3}) {
-
-    useEffect(()=>{
-    setfooter(false)
-    },[])
-
- 
+    // const fetchMyNFTs = axios.get(`https://testnets-api.opensea.io/assets?owner=${accounts[0]}`)
 
     const connectWallet = async () => {
-        try{
-           const accounts = await window.ethereum.request({
-                method: 'eth_requestAccounts',
-              })
-              const web = await new Web3(window.ethereum);
-              setLoginAccount(accounts[0]);
-              setWeb3(web)
+        try {
+            const accounts = await window.ethereum.request({
+                method: "eth_requestAccounts",
+            });
+            const rpcURL = "https://rinkeby.infura.io/v3/14e49a40e7ca44f7b4a9afb62e21c945";
+            const web3 = await new Web3(rpcURL);
+            setLoginAccount(accounts[0]);
+            console.log(accounts[0]);
+            console.log(web3);
+            setWeb3(web3);
+            setIsLogin(true);
+            setfooter(true);
+            //소유한 nft가져오는 법
+            /* await axios.get(`https://testnets-api.opensea.io/assets?owner=${accounts[0]}`).then((result) => {
+                console.log(result.data.assets);
+                result.data.assets.map((item) => {
+                    console.log(item.image_url); // or image_original_url 이건 원본
+                    return item.image_url;
+                });
+            }); */
 
+            //서명 요청 이건 잘 안된다 ㅜ 없애도 괜츈
+            // await web3.eth.getBalance(accounts[0]).then(console.log);
+            // console.log("plus");
+
+            // let from = accounts[0];
+            // let params = [from, "This is for signature"];
+            // let method = "personal_sign";
+            // console.log(from);
+            // web3.utils.toChecksumAddress(from);
+            // try {
+            //     await web3.eth.sign(
+            //         {
+            //             method,
+            //             params,
+            //             from,
+            //         },
+            //         function (err, result) {
+            //             if (!err) {
+            //                 console.log("sometasdfasd");
+            //             } else if (err) {
+            //                 console.log(result);
+            //             }
+            //         }
+            //     );
+            // } catch (e) {
+            //     console.log(e);
+            // }
+        } catch (e) {
+            if (e.code === -32002) {
+                console.log("error! ", e);
+            } else {
+                var win = window.open("https://metamask.io/download.html", "_blank");
+                win.focus();
             }
-            catch(e){
-                if(e.code===-32002){
-                    console.log('error! ',e)
-                }
-                else{
-                    var win = window.open('https://metamask.io/download.html', '_blank');
-                        win.focus();
-                }
-            }
-        
-      };
-      
-    return(
-       <button onClick={()=>connectWallet()}>login</button>
-    )
+        }
+    };
+
+    return (
+        <div className="signInPage">
+            <div>Sign in with your wallet for connecting KiFT</div>
+            <div>
+                <button onClick={() => connectWallet()}>login</button>
+            </div>
+        </div>
+    );
 }
-
 
 export default SignIn;
