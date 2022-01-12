9aafcb2b45c106f83e9f15fdde16d328a41f2923
sonny6786 <3076270s@gmail.com>
2022-01-12 17:44:16 +0900

    modified market page

---
+++ client/src/component/Item.js
@@ -0,0 +1,36 @@
+import "./Item.css";
+import { useState, useEffect } from "react";
+
+function Item({ marketData }) {
+    return (
+        <div className="marketItemContainer">
+            {marketData.map((item) => (
+                <div class="marketItem">
+                    {/* <img src={item.image_url} alt="" /> */}
+                    <div className="imgContainer">
+                        {<img src={item.image_url === "" ? "https://testnets.opensea.io/static/images/placeholder.png" : item.image_url} className="market_image" alt="" />}
+                    </div>
+                    <div className="marketItem_infoContainer">
+                        <div className="marketItem_first-section">{item.name}</div>
+                        <div className="marketItem_price-item-container">
+                            <div className="marketItem_price-item">
+                                <span>Not listed yet</span>
+                                <p className="marketItem_price-text">List price</p>
+                            </div>
+                        </div>
+
+                        <div className="marketItem_user">
+                            <hr className="marketItem_user-divider" />
+                            <div className="marketItem_user-item">
+                                <div className="marketItem_user-title">ARTIST</div>
+                                <div className="marketItem_user">{item.creator.address}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default Item;
--- client/src/component/Market.js
+++ client/src/component/Market.js
@@ -1,16 +1,40 @@
 import "./Market.css";
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Item from "./Item";
 
-function Market({setfooter}) {
+function Market({ setfooter }) {
+    const [marketData, setMarketData] = useState([]);
 
-    useEffect(()=>{
-        setfooter(true)
-        },[])
+    useEffect(() => {
+        setfooter(true);
+    }, []);
 
-    return(
-        null
-    )
+    useEffect(() => {
+        const dataLoad = async () => {
+            await axios
+                .get(`https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=21`)
+                .then((result) => {
+                    console.log(result.data.assets);
+                    setMarketData(result.data.assets);
+                })
+                .catch((err) => console.log(err));
+        };
+        dataLoad();
+    }, []);
+
+    return (
+        <div>
+            <div className="container">
+                <h1 className="marketTitle">Market</h1>
+                <div className="row">
+                    <hr className="divider" />
+                </div>
+
+                <Item marketData={marketData}></Item>
+            </div>
+        </div>
+    );
 }
-
 
 export default Market;
