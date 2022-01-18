f85b31c47af95b3a75cd287b761a9a6aa8e2a081
Sonny <3076270s@gmail.com>
2022-01-18 15:48:52 +0900

    modified setApprovalForAll function

--- client/src/Pages/AboutPage/About.js
+++ client/src/Pages/AboutPage/About.js
@@ -7,229 +7,206 @@ import Web3 from "web3";
 var erc721abi = require("./erc721abi");
 
 function About() {
-  const [sellitem, setSellitem] = useState([]);
-  const [priceSellerPut, setPrice] = useState("");
-  const [showModal, setShowModal] = useState(false);
-  const [traits, setTraits] = useState([]);
-  const [message, setMessage] = useState("");
-  const URLparam = document.location.href.split("mypage/")[1];
+    const [sellitem, setSellitem] = useState([]);
+    const [priceSellerPut, setPrice] = useState("");
+    const [showModal, setShowModal] = useState(false);
+    const [traits, setTraits] = useState([]);
+    const [message, setMessage] = useState("");
+    const URLparam = document.location.href.split("mypage/")[1];
 
-  const closeModal = () => {
-    setShowModal(false);
-  };
-
-  const onChange = (e) => {
-    setPrice(e.target.value);
-    console.log(priceSellerPut);
-  };
-  async function ListItem() {
-    setShowModal(true);
-
-    try {
-      await setApprovalAll().then(async (result) => {
-        await listNFTOnTheMarket;
-      });
-    } catch {}
-  }
-
-  useEffect(() => {
-    loadSellItem();
-  }, []);
-
-  function runEtherscan(e) {
-    var win = window.open(
-      `https://rinkeby.etherscan.io/address/${e.target.outerText}`,
-      "_blank"
-    );
-    win.focus();
-  }
-
-  async function loadSellItem() {
-    const headers = {
-      "Content-Type": "application/json",
-      Accept: "application/json",
+    const closeModal = () => {
+        setShowModal(false);
     };
-    // const params = new URLSearchParams();
-    await axios
-      .post(
-        "http://localhost:3001/searchNFT",
-        {
-          openseaId: URLparam,
-        },
-        headers
-      )
-      .then((result) => {
-        console.log(result);
-        setSellitem(result.data);
-        setTraits(result.data.traits);
-      });
-  }
 
-  async function listNFTOnTheMarket() {
-    const headers = {
-      "Content-Type": "application/json",
-      Accept: "application/json",
+    const onChange = (e) => {
+        setPrice(e.target.value);
+        console.log(priceSellerPut);
     };
-    await axios
-      .post(
-        "http://localhost:3001/listItem",
-        {
-          openseaId: URLparam,
-          price: priceSellerPut,
-          isSale: true,
-        },
-        headers
-      )
-      .then((result) => {
-        if (result.status === 200) {
-          setMessage("Upload your NFT Success!");
-          return result;
-        }
-      })
-      .catch((e) => {
-        //에러를 프론트로 띄워주세요
-        setMessage("listItem request failed! you can check error below");
-      });
-  }
 
-  async function setApprovalAll() {
-    if (typeof window.ethereum !== "undefined") {
-      //여러 wallet 플랫폼중 metaMask로 연결
-
-      const metamaskProvider = await window.ethereum.providers.find(
-        (provider) => provider.isMetaMask
-      );
-      // window.ethereum이 있다면 여기서 window.ethereum이란 메타마스크 설치여부
-      try {
-        const web = new Web3(metamaskProvider);
-
-        web.eth.getAccounts().then(async (account) => {
-          let contract = await new web.eth.Contract(
-            erc721abi,
-            sellitem.contract_address
-          );
-          await contract.methods
-            .setApprovalForAll(
-              "0x543f6fBC5908a8c599C9d9028cF8d4B0026AF1AC", //setapproval 받을 kift.sol 배포 주소
-              true
-            )
-            .send({
-              from: account[0],
-              gas: 2000000,
-              gasPrice: "100000000000",
-            })
-            .then((result) => {
-              setMessage("Approve to KiFT Success!");
-              return result;
-            });
-        });
-      } catch (err) {
-        setMessage("Approve to KiFT Fail!");
-      }
+    async function ListItem() {
+        setMessage("");
+        setShowModal(true);
+        setApprovalAll();
     }
-  }
 
-  return (
-    <div className="about_main">
-      {showModal && (
-        <NotifyModal
-          showModal={showModal}
-          closeModal={closeModal}
-          message={message}
-        ></NotifyModal>
-      )}
+    useEffect(() => {
+        loadSellItem();
+    }, []);
 
-      <div className="sell_bar"></div>
-      <div className="middle2">
-        <div>
-          <div className="nft_name_box">
-            <div className="nft_name">{sellitem.name}</div>
-            <div className="nft_owned">
-              Owned by <span className="owned_address">you</span>
+    function runEtherscan(e) {
+        var win = window.open(`https://rinkeby.etherscan.io/address/${e.target.outerText}`, "_blank");
+        win.focus();
+    }
+
+    async function loadSellItem() {
+        const headers = {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        };
+        // const params = new URLSearchParams();
+        await axios
+            .post(
+                "http://localhost:3001/searchNFT",
+                {
+                    openseaId: URLparam,
+                },
+                headers
+            )
+            .then((result) => {
+                console.log(result);
+                setSellitem(result.data);
+                setTraits(result.data.traits);
+            });
+    }
+
+    async function listNFTOnTheMarket() {
+        const headers = {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        };
+        await axios
+            .post(
+                "http://localhost:3001/listItem",
+                {
+                    openseaId: URLparam,
+                    price: priceSellerPut,
+                    isSale: true,
+                },
+                headers
+            )
+            .then((result) => {
+                if (result.status === 200) {
+                    setMessage("Upload your NFT Success!");
+                    return result;
+                }
+            })
+            .catch((e) => {
+                //에러를 프론트로 띄워주세요
+                setMessage("listItem request failed! you can check error below");
+            });
+    }
+
+    async function setApprovalAll() {
+        if (typeof window.ethereum !== "undefined") {
+            //여러 wallet 플랫폼중 metaMask로 연결
+
+            const metamaskProvider = await window.ethereum.providers.find((provider) => provider.isMetaMask);
+            // window.ethereum이 있다면 여기서 window.ethereum이란 메타마스크 설치여부
+            try {
+                const web = new Web3(metamaskProvider);
+
+                web.eth.getAccounts().then(async (account) => {
+                    let contract = await new web.eth.Contract(erc721abi, sellitem.contract_address);
+                    await contract.methods
+                        .setApprovalForAll(
+                            "0x543f6fBC5908a8c599C9d9028cF8d4B0026AF1AC", //setapproval 받을 kift.sol 배포 주소
+                            true
+                        )
+                        .send({
+                            from: account[0],
+                            gas: 100000,
+                            gasPrice: "10000000000",
+                        })
+                        .then((result) => {
+                            setMessage("Approve to KiFT Success!", result.blockHash);
+                            console.log("This is success result--->>>", result);
+                            console.log("This is Hash ", result.blockHash);
+                            return result;
+                        })
+                        .catch((err) => {
+                            console.log("this is whole error message", err);
+                            console.log("this is error message----->>>>", err.message);
+                            setMessage(err.message);
+                        });
+                });
+            } catch (err) {
+                setMessage("Approve to KiFT Fail!");
+            }
+        }
+    }
+
+    return (
+        <div className="about_main">
+            {showModal && <NotifyModal showModal={showModal} closeModal={closeModal} message={message}></NotifyModal>}
+
+            <div className="sell_bar"></div>
+            <div className="middle2">
+                <div>
+                    <div className="nft_name_box">
+                        <div className="nft_name">{sellitem.name}</div>
+                        <div className="nft_owned">
+                            Owned by <span className="owned_address">you</span>
+                        </div>
+                    </div>
+
+                    <img className="nft_image" src={sellitem.image_url} />
+                </div>
+                <div className="simple_description">
+                    <div className="center_description">
+                        <div className="description_title">
+                            Description
+                            <div className="price_input_box">
+                                <input className="price" placeholder="Amount" value={priceSellerPut} onChange={onChange} />
+                                <button className="sell_button" onClick={ListItem}>
+                                    Sell
+                                </button>
+                            </div>
+                        </div>
+                        <div className="description2">
+                            created by{" "}
+                            <span onClick={(e) => runEtherscan(e)} className="description_address">
+                                {"패치중"}
+                            </span>
+                        </div>
+                    </div>
+                    <div className="center_properties">
+                        <div className="properties_title">Properties</div>
+                        <div className="properties">
+                            {traits.map((prop, index) => {
+                                return (
+                                    <div className="props" key={index}>
+                                        <div className="props_1">{prop.trait_type}</div>
+                                        <div className="props_2">{prop.value}</div>
+                                        <div className="props_3">100% have this trait</div>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
+
+                    <div className="center_details">
+                        <div className="details_title">Details</div>
+                        <div className="details">
+                            <div className="detail_menu">
+                                <span>contract Address</span>
+                                <span className="right_end addoption" onClick={(e) => runEtherscan(e)}>
+                                    {sellitem.contract_address}
+                                </span>
+                            </div>
+                            <div className="detail_menu">
+                                <span>token id</span>
+                                <span className="right_end">{sellitem.NFT_Token_id}</span>
+                            </div>
+                            <div className="detail_menu">
+                                <span>token standard</span>
+                                <span className="right_end">{sellitem.schema_name}</span>
+                            </div>
+                            <div className="detail_menu">
+                                <span>blockchain</span>
+                                <span className="right_end">Rinkeby</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-
-          <img className="nft_image" src={sellitem.image_url} />
+            <Accordion defaultActiveKey="0">
+                <Accordion.Item className="acc_item" eventKey="0">
+                    <Accordion.Header className="acc_header">Price History</Accordion.Header>
+                    <Accordion.Body>NOT UPDATING..</Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
         </div>
-        <div className="simple_description">
-          <div className="center_description">
-            <div className="description_title">
-              Description
-              <div className="price_input_box">
-                <input
-                  className="price"
-                  placeholder="Amount"
-                  value={priceSellerPut}
-                  onChange={onChange}
-                />
-                <button className="sell_button" onClick={ListItem}>
-                  Sell
-                </button>
-              </div>
-            </div>
-            <div className="description2">
-              created by{" "}
-              <span
-                onClick={(e) => runEtherscan(e)}
-                className="description_address"
-              >
-                {"패치중"}
-              </span>
-            </div>
-          </div>
-          <div className="center_properties">
-            <div className="properties_title">Properties</div>
-            <div className="properties">
-              {traits.map((prop, index) => {
-                return (
-                  <div className="props" key={index}>
-                    <div className="props_1">{prop.trait_type}</div>
-                    <div className="props_2">{prop.value}</div>
-                    <div className="props_3">100% have this trait</div>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-
-          <div className="center_details">
-            <div className="details_title">Details</div>
-            <div className="details">
-              <div className="detail_menu">
-                <span>contract Address</span>
-                <span
-                  className="right_end addoption"
-                  onClick={(e) => runEtherscan(e)}
-                >
-                  {sellitem.contract_address}
-                </span>
-              </div>
-              <div className="detail_menu">
-                <span>token id</span>
-                <span className="right_end">{sellitem.NFT_Token_id}</span>
-              </div>
-              <div className="detail_menu">
-                <span>token standard</span>
-                <span className="right_end">{sellitem.schema_name}</span>
-              </div>
-              <div className="detail_menu">
-                <span>blockchain</span>
-                <span className="right_end">Rinkeby</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <Accordion defaultActiveKey="0">
-        <Accordion.Item className="acc_item" eventKey="0">
-          <Accordion.Header className="acc_header">
-            Price History
-          </Accordion.Header>
-          <Accordion.Body>NOT UPDATING..</Accordion.Body>
-        </Accordion.Item>
-      </Accordion>
-    </div>
-  );
+    );
 }
 
 export default About;
