7e67e46fc3ab4d798d2f4e55599fb81ba4285817
BuilderBoba <troh96@gmail.com>
2022-01-25 16:58:53 +0900

    added claim frontend

--- client/src/Pages/ClaimPage/Claim.js
+++ client/src/Pages/ClaimPage/Claim.js
@@ -1,12 +1,64 @@
 import Web3 from "web3";
 import dotenv from "dotenv";
 import axios from "axios";
+import { useState, useEffect } from "react";
+import pic from "./background.jpeg";
 import "./Claim.css";
 
 dotenv.config();
 var KiFTTokenabi = require("./KiFTTokenabi");
 
-function Claim() {
+function Claim(isLogin) {
+
+  const [status, setStatus] = useState("");
+  const [account, setAccount] = useState("");
+  const [token, setToken] = useState(0);
+
+  function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  }
+
+  useEffect(async () => {
+    if (typeof window.ethereum === undefined) {
+      setStatus(
+        "Wallet not connected. Please connect your Metamask wallet to browser."
+      );
+    } else {
+      if (typeof window.ethereum.providers === "undefined") {
+        var metamaskProvider = window.ethereum;
+      } else {
+        var metamaskProvider = window.ethereum.providers.find(
+          (provider) => provider.isMetaMask
+        );
+      }
+
+      try {
+        const web = new Web3(metamaskProvider);
+        const accounts = await metamaskProvider.request({
+          method: "eth_requestAccounts",
+        });
+        setAccount(accounts[0].toLowerCase());
+        web.eth.getAccounts().then(async (account) => {
+          await axios
+            .post("http://localhost:3001/findUser", {
+              address: account[0].toLowerCase()
+            })
+            .then((result) => {
+              var token = parseInt(result.data.data.points)*7;
+              setToken(parseInt(result.data.data.points));
+              setStatus(
+                "Wallet Connected: You are eligible for "+
+                token+
+                " tokens"
+              );
+            });
+        });
+      } catch (err) {
+        console.log("Error: ", err);
+      }
+    }
+  }, []);
+
   function claimKiFTToken() {
     if (typeof window.ethereum !== "undefined") {
       //여러 wallet 플랫폼중 metaMask로 연결
@@ -15,7 +67,9 @@ function Claim() {
         var metamaskProvider = window.ethereum;
         console.log("메타마스크만 다운되어있는 것 처리===>", metamaskProvider);
       } else {
-        var metamaskProvider = window.ethereum.providers.find((provider) => provider.isMetaMask);
+        var metamaskProvider = window.ethereum.providers.find(
+          (provider) => provider.isMetaMask
+        );
         console.log("여러개 지갑 처리 ==>", metamaskProvider);
       }
 
@@ -25,10 +79,13 @@ function Claim() {
           //계정 조회후 포인트 받아옴
           await axios
             .post("http://localhost:3001/findUser", {
-              address: account[0].toLowerCase(),
+              address: account[0].toLowerCase()
             })
             .then((result) => {
-              console.log("how many got token======>>>>", result.data.data.points);
+              console.log(
+                "how many got token======>>>>",
+                result.data.data.points
+              );
               return result.data.data.points;
             })
             .then(async (point) => {
@@ -36,13 +93,16 @@ function Claim() {
               if (point > 0) {
                 let numbersUserCanClaim = point * 7 * 1000000000000000000;
                 console.log(numbersUserCanClaim.toString());
-                let contract = await new web.eth.Contract(KiFTTokenabi, process.env.REACT_APP_KIFT_TOKEN_CONTRACT_ADDRESS);
+                let contract = await new web.eth.Contract(
+                  KiFTTokenabi,
+                  process.env.REACT_APP_KIFT_TOKEN_CONTRACT_ADDRESS
+                );
                 await contract.methods
                   .mintToken(account[0], numbersUserCanClaim.toString())
                   .send({
                     from: account[0],
                     gas: 100000,
-                    gasPrice: "10000000000",
+                    gasPrice: "10000000000"
                   })
                   .then(async (receipt) => {
                     console.log(receipt);
@@ -50,23 +110,34 @@ function Claim() {
                       //민트 성공하면 디비 초기화 !!
                       await axios
                         .post("http://localhost:3001/initializePoints", {
-                          address: account[0].toLowerCase(),
+                          address: account[0].toLowerCase()
                         })
                         .then((result) => {
-                          console.log("requesting initialize points to zero successed!!!! =====>>", result);
+                          console.log(
+                            "requesting initialize points to zero successed!!!! =====>>",
+                            result
+                          );
                         });
                     } else {
                       //민트 실패
-                      console.log("there's no points you got, it failed to claim Tokens");
+                      console.log(
+                        "there's no points you got, it failed to claim Tokens"
+                      );
                     }
                   })
                   .catch((err) => {
-                    console.log("Error occured when minting Tokens!!!!!======>>", err);
+                    console.log(
+                      "Error occured when minting Tokens!!!!!======>>",
+                      err
+                    );
                   });
               }
             })
             .catch((err) => {
-              console.log("Error occured when sending request about POINTS!! ====>>", err);
+              console.log(
+                "Error occured when sending request about POINTS!! ====>>",
+                err
+              );
             });
         });
       } catch (err) {
@@ -77,8 +148,11 @@ function Claim() {
 
   return (
     <div className="claimPageContainer">
-      <div className="claimPageBlock"></div>
-      <button onClick={claimKiFTToken}>클레임</button>
+        <div className="claimTitle"> Claim Your KiFT Tokens</div>
+        {isLogin ? <div className="account">{"Your Address: \n" + account}</div> : null}
+        <div className="amount">{status}</div>
+        {token > 0 ? <button className="claim" onClick={claimKiFTToken}>Claim Tokens</button> : <button className="claim" disabled> No Claimable Tokens </button>}
+        <div className="image"></div>
     </div>
   );
 }
