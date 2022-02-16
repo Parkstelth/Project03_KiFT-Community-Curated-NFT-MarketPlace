60118d218f8e94929db9107e7175bd3b15d9aa78
Parkstelth <stelthicmaster@gmail.com>
2022-02-16 23:03:36 +0900

    final

--- client/src/Pages/SigninPage/SignIn.js
+++ client/src/Pages/SigninPage/SignIn.js
@@ -9,71 +9,79 @@ function SignIn({ setfooter, setLoginAccount, setIsLogin, setIsKaikas, isKaikas 
   }, []);
 
   const connectWallet = async () => {
-    if (typeof window.ethereum.providers === "undefined") {
-      var metamaskProvider = window.ethereum;
-      console.log("메타마스크만 다운되어있는 것 처리===>", metamaskProvider);
-    } else {
-      var metamaskProvider = window.ethereum.providers.find((provider) => provider.isMetaMask);
-      console.log("여러개 지갑 처리 ==>", metamaskProvider);
-    }
-
-    try {
-      const accounts = await metamaskProvider.request({
-        method: "eth_requestAccounts",
-      });
-
-      setLoginAccount(accounts[0].toLowerCase());
-
-      setIsLogin(true);
-      setfooter(true);
-
-      const headers = {
-        "Content-Type": "application/x-www-form-urlencoded",
-        Accept: "*/*",
-      };
-
-      const params = new URLSearchParams();
-      params.append("loginAddress", accounts[0].toLowerCase());
-
-      await axios.post("http://localhost:3001/sign", params, { headers }).then((res) => {
-        console.log(res);
-      });
-
-      setIsKaikas(false);
-      document.location.href = "/market";
-    } catch (e) {
-      if (typeof window.ethereum === "undefined") {
-        var win = window.open("https://metamask.io/download.html", "_blank");
-        win.focus();
+    if (window.ethereum !== undefined) {
+      if (typeof window.ethereum.providers === "undefined") {
+        var metamaskProvider = window.ethereum;
+        console.log("메타마스크만 다운되어있는 것 처리===>", metamaskProvider);
       } else {
-        console.log("error! ", e);
+        var metamaskProvider = window.ethereum.providers.find((provider) => provider.isMetaMask);
+        console.log("여러개 지갑 처리 ==>", metamaskProvider);
       }
+
+      try {
+        const accounts = await metamaskProvider.request({
+          method: "eth_requestAccounts",
+        });
+
+        setLoginAccount(accounts[0].toLowerCase());
+
+        setIsLogin(true);
+        setfooter(true);
+
+        const headers = {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Accept: "*/*",
+        };
+
+        const params = new URLSearchParams();
+        params.append("loginAddress", accounts[0].toLowerCase());
+
+        await axios.post("http://localhost:3001/sign", params, { headers }).then((res) => {
+          console.log(res);
+        });
+
+        setIsKaikas(false);
+        document.location.href = "/market";
+      } catch (e) {
+        if (typeof window.ethereum === "undefined") {
+          var win = window.open("https://metamask.io/download.html", "_blank");
+          win.focus();
+        } else {
+          console.log("error! ", e);
+        }
+      }
+    } else {
+      alert("Please download Metamask!");
     }
   };
 
   const connectKaikas = async () => {
-    await window.klaytn.enable();
-    console.log(window.klaytn.selectedAddress);
+    if (window.klaytn !== undefined) {
+      await window.klaytn.enable();
+      console.log(window.klaytn.selectedAddress);
 
-    const caver = new Caver(window.klaytn);
-    caver.klay.getAccounts().then(async (account) => {
-      setLoginAccount(account[0].toLowerCase());
-      setIsLogin(true);
-      setfooter(true);
-      setIsKaikas(true);
-      const headers = {
-        "Content-Type": "application/x-www-form-urlencoded",
-        Accept: "*/*",
-      };
+      const caver = new Caver(window.klaytn);
+      caver.klay.getAccounts().then(async (account) => {
+        setLoginAccount(account[0].toLowerCase());
+        setIsLogin(true);
+        setfooter(true);
+        setIsKaikas(true);
+        const headers = {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Accept: "*/*",
+        };
 
-      const params = new URLSearchParams();
-      params.append("loginAddress", account[0].toLowerCase());
-      params.append("Chain", "baobab");
+        const params = new URLSearchParams();
+        params.append("loginAddress", account[0].toLowerCase());
+        params.append("Chain", "baobab");
 
-      await axios.post("http://localhost:3001/sign", params, { headers }).then((res) => {
-        document.location.href = "/";
+        await axios.post("http://localhost:3001/sign", params, { headers }).then((res) => {
+          document.location.href = "/";
+        });
       });
-    });
+    } else {
+      alert("Please download Kaikas!");
+    }
   };
   return (
     <div className="signInPageContainer">
